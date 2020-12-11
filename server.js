d124076bcf986ac25ae4736dc1b7b809d51a68bb
David <davidaccomazzo@gmail.com>
2020-12-10 17:37:34 -0700

    adding edit functionality

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/api/all_notes', (req, res) => {
     res.status(200).send(all_notes)
 })
 
+// Fetch a single note
 app.get('/api/single_note/:id', (req, res) => {
     const single_note = db.prepare(
         "SELECT * FROM NOTES WHERE id = ?"
@@ -34,10 +35,21 @@ app.get('/api/single_note/:id', (req, res) => {
     res.status(200).send(single_note)
 })
 
+// Create a note
 app.post('/api/add_note', (req, res) => {
     console.log(req.body)
     const note = db.prepare(
         "INSERT INTO notes (title, body) VALUES (?, ?)"
     ).run(req.body.title, req.body.body)
     res.status(200).send(note)
+})
+
+
+// Update a note
+app.post('/api/update_note/', (req, res) => {
+    console.log(req.body)
+    const updated_note = db.prepare(
+        "UPDATE notes SET title = ?, body = ? WHERE id = ?"
+    ).run(req.body.title, req.body.body, req.body.id)
+    res.status(200).send(updated_note)
 })
